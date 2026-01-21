16232b35aceb78db686a6a7dcffad5befb9f3949
kavalirahul <kavalirahul2927@gmail.com>
2026-01-22 00:34:53 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { applyMiddleware } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import Store from './store';
 import reducer from './Reducer'
 const middleware = [thunk];
 
